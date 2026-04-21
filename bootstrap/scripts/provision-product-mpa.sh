dc20face8ce255a7873074ba40e1d9b2657be757
Giosue Tironi <giosue.tironi@helixcloud.ch>
2026-04-21 15:10:33 +0200

    Fix: remove duplicated read bucket permission

--- bootstrap/scripts/provision-product-mpa.sh
+++ bootstrap/scripts/provision-product-mpa.sh
@@ -128,7 +128,6 @@ else
       "Key=ProjectName,Value=${CB_PROJECT_NAME}" \
       "Key=CreateBucket,Value=true" \
       "Key=CBS3SourceBucket,Value=${OPERATION_SOURCE_BUCKET}" \
-      "Key=S3ReadBuckets,Value=${OPERATION_SOURCE_BUCKET}" \
     --query 'RecordDetail.ProvisionedProductId' \
     --output text)
   echo "Provisioning started: ${MPA_PP_ID}"
