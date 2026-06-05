24ad4b62486244f9847717f6752465e619bbda78
Daniele Salvagni <daniele.salvagni@helixcloud.ch>
2026-06-05 18:27:07 +0200

    feat: export ou names and arns

--- landing-zone/projects/workload-ous/terraform/outputs.tf
+++ landing-zone/projects/workload-ous/terraform/outputs.tf
@@ -6,3 +6,21 @@ output "ou_ids" {
     { for path, ou in aws_organizations_organizational_unit.level_3 : path => ou.id },
   )
 }
+
+output "ou_names" {
+  description = "Map of OU path to OU name."
+  value = merge(
+    { for path, ou in aws_organizations_organizational_unit.level_1 : path => ou.name },
+    { for path, ou in aws_organizations_organizational_unit.level_2 : path => ou.name },
+    { for path, ou in aws_organizations_organizational_unit.level_3 : path => ou.name },
+  )
+}
+
+output "ou_arns" {
+  description = "Map of OU path to OU ARN."
+  value = merge(
+    { for path, ou in aws_organizations_organizational_unit.level_1 : path => ou.arn },
+    { for path, ou in aws_organizations_organizational_unit.level_2 : path => ou.arn },
+    { for path, ou in aws_organizations_organizational_unit.level_3 : path => ou.arn },
+  )
+}
