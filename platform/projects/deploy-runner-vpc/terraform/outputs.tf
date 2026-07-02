a230a552cea5033c23bd7df9ec7b0647006d37a7
Daniele Salvagni <daniele.salvagni@helixcloud.ch>
2026-07-02 17:41:07 +0200

    feat: deploy-runner-vpc project

---
+++ platform/projects/deploy-runner-vpc/terraform/outputs.tf
@@ -0,0 +1,14 @@
+output "project_name" {
+  description = "Name of the VPC-attached CodeBuild project. Use as `runner` value in pipeline steps."
+  value       = aws_codebuild_project.runner.name
+}
+
+output "project_arn" {
+  description = "ARN of the CodeBuild project."
+  value       = aws_codebuild_project.runner.arn
+}
+
+output "security_group_id" {
+  description = "Security group ID attached to the CodeBuild ENI."
+  value       = aws_security_group.codebuild.id
+}
